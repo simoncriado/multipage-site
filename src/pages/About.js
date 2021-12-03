5cc159ee1230e8ed5aa21ac0f212952d5189e1e6
simon <scriado.f@gmail.com>
2021-12-03 11:38:11 +0100

    Created the router functionality, added links and navLinks, fetched some data from json and added a redirect in case of error

---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div>
+            <h2>About</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In id a nisi minus, architecto consectetur! Voluptatem officia recusandae minus perferendis fugiat. Est dolorem dignissimos dolor porro, doloribus eligendi. Optio, excepturi?</p>
+        </div>
+    )
+}
---
+++ src/pages/Contact.js
@@ -0,0 +1,8 @@
+export default function Contact() {
+    return (
+        <div>
+            <h2>Contact</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In id a nisi minus, architecto consectetur! Voluptatem officia recusandae minus perferendis fugiat. Est dolorem dignissimos dolor porro, doloribus eligendi. Optio, excepturi?</p>
+        </div>
+    )
+}
