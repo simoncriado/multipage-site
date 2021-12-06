38dd78a50c31241be352a17761f9f6af29b43f8c
simon <scriado.f@gmail.com>
2021-12-06 15:30:55 +0100

    added redirect route in case of error fetsching data or wrong url. Brief view of query paramenters

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,16 @@
+import { useLocation } from "react-router-dom"
+
 export default function Contact() {
+    const queryString = useLocation().search
+
+    const queryParams = new URLSearchParams(queryString)
+    const name = queryParams.get("name")
+
+    // ?name=mario
+
     return (
         <div>
-            <h2>Contact</h2>
+            <h2>Hey {name}, contact us here...</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In id a nisi minus, architecto consectetur! Voluptatem officia recusandae minus perferendis fugiat. Est dolorem dignissimos dolor porro, doloribus eligendi. Optio, excepturi?</p>
         </div>
     )
